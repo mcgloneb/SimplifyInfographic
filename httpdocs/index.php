ec56b144eb10cf4f6c5e4220489f7cb2e85bb6f3
Dan Connor <danconnor@gmail.com>
2017-11-02 11:08:58 +0000

    Deleted temp images, updated SCSS

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -25,10 +25,12 @@
         </div>
 
         <!-- intro line start -->
-        <div class="container-fluid scene--fullwidth">
-            <div class="row scene scene--no-margin">
-                <div class="col-4 scene__introline">
-                    Four steps to improved application performance with machine learning
+        <div class="outer-container">
+            <div class="container">
+                <div class="row scene scene--no-margin">
+                    <div class="col-4 scene__introline">
+                        Four steps to improved application performance with machine learning
+                    </div>
                 </div>
             </div>
         </div>
@@ -49,7 +51,7 @@
                 </div>   
             </div> 
 
-            <div class="row scene scene__one scene--no-margin">
+            <div class="row scene scene__one scene--no-margin scene--padding">
                 <div class="col-md-4 offset-md-1 col-sm-12 text-center" id="s1_trigger">
                         <figure class="static-chart">
                             <svg width="200" height="200">:
@@ -103,11 +105,14 @@
                     <div class="row">
                         <div class="col-md-7 offset-md-1 col-sm-12 scene--bordered">
                             <div class="row scene">
-                                <div class="col-md-8 col-sm-12">
+                                <div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1">
                                     <p><strong>Why?</strong> Existing monitoring solutions are slow and create too much data</p>
                                 </div>
+                                <div class="col-md-10 offset-md-2 col-sm-10 offset-sm-1">
+                                    <p><strong>The result:</strong> <span>Only 6%</span> of companies completely trust their monitoring strategy<sup>1</sup></p>
+                                </div>
                             </div>
-                            <p><strong>The result:</strong> <span>Only 6%</span> of companies completely trust their monitoring strategy<sup>1</sup></p>
+                            
                         </div>
                         <div class="col-md-3 col-sm-12 text-center scene__graphic scene--bordered flex-container">
                             <img src="./images/temp_dial.png" />
@@ -155,7 +160,7 @@
                     <div class="row text-center scene__graphic"> 
                         <div class="col-md-4 col-sm-12" ><?php include('./images/svg/big_data.php'); ?><p><span>80%</span><br />of production applications will run in the cloud</p></div>
                         <div class="col-md-4 col-sm-12"><img src="./images/temp_scene3_icon2.png" /><p><span>80%</span><br />of IT budgets will be spent on business innovation, and only</p></div>
-                        <div class="col-md-4 col-sm-12"><?php include('./images/svg/laptop_with_spanner.php'); ?><p><span>20%</span><br />will go towards system maintenance</p></div>    
+                        <div class="col-md-4 col-sm-12" id="laptopspanner"><?php include('./images/svg/laptop_with_spanner.php'); ?><p><span>20%</span><br />will go towards system maintenance</p></div>    
                     </div>
 
 
@@ -190,18 +195,17 @@
                 
                 <div class="col-12 scene__body">
                     <div class="row">
-                        <div class="col-6 scene__graphic" id="s4_trigger">
+                        <div class="col-md-5 offset-md-1 cols-xs-12 scene__graphic flex-container" id="s4_trigger">
                             <img src="./images/temp_scene4_magnifyingglass.png" />
                         </div>
-                        <div class="col-6">
+                        <div class="col-md-5 offset-md-1 col-sm-12 ">
                             <p><span>75%</span><br />of heavy users of multiple cloud solutions have already implemented service-level monitoring and reporting across their full mix of public, private and hybrid cloud resources<sup>3</sup>.</p>
                             <p>Oracle Management Cloud (OMC) intelligently identifies performance issues across all your cloud resources using four types of machine learning algorithm:</p>
                         </div>
                     </div>
 
                     <div class="row scene__callout">
-                        <div class="col-1">&nbsp;</div>
-                        <div class="col-7">
+                        <div class="col-md-7 offset-md-1 col-sm-12">
                             <ul>
                                 <li>Correlating data sets that are too large for humans to evaluate</li>
                                 <li>Clustering data sets to automatically identify problems</li>
@@ -209,14 +213,14 @@
                                 <li>Predicting future behaviour to enable intelligent decisions</li>
                             </ul>
                         </div>
-                        <div class="col-4 flex-container scene__graphic">
+                        <div class="col-md-4 col-sm-12 flex-container scene__graphic">
                             <?php include('./images/svg/tree.php'); ?>
                         </div>
                     </div>
 
                     <div class="row scene__body">
-                        <div class="col-md-1 arrow_icon flex-container"><img src="./images/temp_scene4_arrow.png" /></div>
-                        <div class="col-md-8">Machine learning is essential for effective application performance monitoring.</div>
+                        <div class="col-md-2 offset-md-1 col-sm-2 scene__graphic flex-container"><img src="./images/temp_scene4_arrow.png" class="arrow_icon" /></div>
+                        <div class="col-md-8 col-sm-9"><p class="no-margin">Machine learning is essential for effective application performance monitoring.</p></div>
                     </div>
 
                 </div>
